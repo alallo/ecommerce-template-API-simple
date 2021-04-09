a2b42978ff6955872b355f625295045d7a8568fc
Alessandro Lallo <alessandro.lallo@gmail.com>
2021-04-09 11:14:36 +0100

    trying to get terraform output in the pipeline

--- Deploy/terraform/outputs.tf
+++ Deploy/terraform/outputs.tf
@@ -1,14 +1,14 @@
-output "id" {
+output "kvid" {
   value       = azurerm_key_vault.ecommerce_kv.id
   description = "The ID of the Key Vault."
 }
 
-output "name" {
+output "kvname" {
   value       = azurerm_key_vault.ecommerce_kv.name
   description = "The name of the Key Vault."
 }
 
-output "uri" {
+output "kvuri" {
   value       = azurerm_key_vault.ecommerce_kv.vault_uri
   description = "The URI of the Key Vault."
 }
