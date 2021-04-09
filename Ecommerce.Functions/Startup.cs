e37a25e40fd9333e59666184f6a1d58cd7d2d879
Alessandro Lallo <alessandro.lallo@gmail.com>
2021-04-09 10:57:09 +0100

    Get settings from KV

--- Ecommerce.Functions/Startup.cs
+++ Ecommerce.Functions/Startup.cs
@@ -1,9 +1,11 @@
-using System;
 using System.IO;
 using Ecommerce.Application;
 using Ecommerce.EmailService;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(Ecommerce.Functions.Startup))]
@@ -23,7 +25,22 @@ namespace Ecommerce.Functions
             var configBuilder = new ConfigurationBuilder();
             configBuilder.SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false);
-            var config = configBuilder.AddEnvironmentVariables().Build();
+             var config = configBuilder.AddEnvironmentVariables().Build();
+             
+            var enableKeyVault = config["keyVaultName"] != null;
+
+            if (enableKeyVault)
+            {
+                var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                var keyVaultClient = new KeyVaultClient(
+                    new KeyVaultClient.AuthenticationCallback(
+                        azureServiceTokenProvider.KeyVaultTokenCallback));
+
+                config = configBuilder
+                    .AddAzureKeyVault($"https://{config["keyVaultName"]}.vault.azure.net", keyVaultClient,
+                        new DefaultKeyVaultSecretManager())
+                    .Build();
+            }
 
             services.AddSingleton<IEmailService, SendGridService>();
             services.AddSingleton<IOrderService, OrderService>();
