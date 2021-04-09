47ff7a9bee04b84e29dc650eb0b501d5e3f38289
Alessandro Lallo <alessandro.lallo@gmail.com>
2021-04-09 09:09:10 +0100

    output still wrong

--- Deploy/terraform/outputs.tf
+++ Deploy/terraform/outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
-  value       = azurerm_key_vault.ecommerce_kv[0].id
+  value       = azurerm_key_vault.ecommerce_kv.id
   description = "The ID of the Key Vault."
 }
 
 output "name" {
-  value       = azurerm_key_vault.ecommerce_kv[0].name
+  value       = azurerm_key_vault.ecommerce_kv.name
   description = "The name of the Key Vault."
 }
 
 output "uri" {
-  value       = azurerm_key_vault.ecommerce_kv[0].vault_uri
+  value       = azurerm_key_vault.ecommerce_kv.vault_uri
   description = "The URI of the Key Vault."
 }
